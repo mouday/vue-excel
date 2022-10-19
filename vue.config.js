5bf79f905fbccdf37b9a1cfb1425db20200c8884
pengshiyu <1940607002@qq.com>
2022-10-19 10:11:09 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 // vue.config.js
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-print/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-excel/' : '/',
 
   configureWebpack: {
     resolve: {
